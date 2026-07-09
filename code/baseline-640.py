1b87aa67e1b2e6bf0378e1e7d5a684871a296ebb
Xin-LY <1848668707@qq.com>
2026-07-09 14:57:10 +0800

    Add files via upload

---
+++ code/baseline-640.py
@@ -0,0 +1,15 @@
+
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    # 基线YOLOv8m 416分辨率 50轮训练
+    model = YOLO("yolov8m.pt")
+    model.train(
+        data="./NEU-DET/data.yaml",
+        imgsz=640,
+        epochs=50,
+        batch=8,
+        device=0,
+        amp=False,
+        workers=0
+    )
